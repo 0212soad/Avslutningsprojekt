a25f6f01961dfbcbf21d9782f6cae090660ff018
Adam Sobczyk <0212soad@jonkoping.se>
2021-05-24 15:29:06 +0200

    Sjunde

--- projekt.js
+++ projekt.js
@@ -1,3 +1,4 @@
+var chart = 0;
 $(document).ready(function() {
     $(".zoom").on("click", function() {
         $(".a").each(function() {
@@ -66,139 +67,57 @@ function toggleNav2() {
 
 //Grafer
 
-function ShowGraph() {
+function ShowChart() {
+    openNav2();
     $(".ct-chart").addClass('animate');
-    $(".openbtn2").show();
-}
-
-var type = null;
-
-function MakeGraph() {
-    var chart = new Chartist.Line('.ct-chart', {
+    $(".openbtn2").addClass('animate');
+    var data = {
         series: [
             [
-                { x: 1, y: 100 },
-                { x: 2, y: 50 },
-                { x: 3, y: 25 },
-                { x: 5, y: 12.5 },
-                { x: 8, y: 6.25 }
+                { x: 0, y: 1 },
+                { x: 2, y: 2 },
+                { x: 4, y: 3 },
+                { x: 6, y: 4 },
+                { x: 8, y: 5 }
             ]
         ]
-    }, {
+    }
+    var options = {
         axisX: {
             type: Chartist.AutoScaleAxis,
         },
         low: 0,
-        width: 25 + '%',
-        height: 25 + '%'
-    });
-
-    // Let's put a sequence number aside so we can use it in the event callbacks
-    var seq = 0,
-        delays = 80,
-        durations = 500;
-
-    // Once the chart is fully created we reset the sequence
-    chart.on('created', function() {
-        seq = 0;
-    });
-
-    // On each drawn element by Chartist we use the Chartist.Svg API to trigger SMIL animations
-    chart.on('draw', function(data) {
-        seq++;
-        if (data.type === 'line') {
-            // If the drawn element is a line we do a simple opacity fade in. This could also be achieved using CSS3 animations.
-            data.element.animate({
-                opacity: {
-                    begin: seq * delays + 1000,
-                    dur: durations,
-                    from: 0,
-                    to: 1
-                }
-            });
-        } else if (data.type === 'label' && data.axis === 'x') {
-            data.element.animate({
-                y: {
-                    begin: seq * delays,
-                    dur: durations,
-                    from: data.y + 100,
-                    to: data.y,
-                    // We can specify an easing function from Chartist.Svg.Easing
-                    easing: 'easeOutQuart'
-                }
-            });
-        } else if (data.type === 'label' && data.axis === 'y') {
-            data.element.animate({
-                x: {
-                    begin: seq * delays,
-                    dur: durations,
-                    from: data.x - 100,
-                    to: data.x,
-                    easing: 'easeOutQuart'
-                }
-            });
-        } else if (data.type === 'point') {
-            data.element.animate({
-                x1: {
-                    begin: seq * delays,
-                    dur: durations,
-                    from: data.x - 10,
-                    to: data.x,
-                    easing: 'easeOutQuart'
-                },
-                x2: {
-                    begin: seq * delays,
-                    dur: durations,
-                    from: data.x - 10,
-                    to: data.x,
-                    easing: 'easeOutQuart'
-                },
-                opacity: {
-                    begin: seq * delays,
-                    dur: durations,
-                    from: 0,
-                    to: 1,
-                    easing: 'easeOutQuart'
-                }
-            });
-        } else if (data.type === 'grid') {
-            // Using data.axis we get x or y which we can use to construct our animation definition objects
-            var pos1Animation = {
-                begin: seq * delays,
-                dur: durations,
-                from: data[data.axis.units.pos + '1'] - 30,
-                to: data[data.axis.units.pos + '1'],
-                easing: 'easeOutQuart'
-            };
-
-            var pos2Animation = {
-                begin: seq * delays,
-                dur: durations,
-                from: data[data.axis.units.pos + '2'] - 100,
-                to: data[data.axis.units.pos + '2'],
-                easing: 'easeOutQuart'
-            };
-
-            var animations = {};
-            animations[data.axis.units.pos + '1'] = pos1Animation;
-            animations[data.axis.units.pos + '2'] = pos2Animation;
-            animations['opacity'] = {
-                begin: seq * delays,
-                dur: durations,
-                from: 0,
-                to: 1,
-                easing: 'easeOutQuart'
-            };
-
-            data.element.animate(animations);
-        }
-    });
+        width: "22%",
+        height: "25%",
+        showLine: false,
+        showLabel: false,
+        showPoint: false,
+    }
+    chart = new Chartist.Line('.ct-chart', data, options);
 }
 
 function Bar() {
-    MakeGraph();
-}
-
-function Line() {
-    MakeGraph();
+    var data = {
+        series: [
+            [
+                { x: 0, y: 1 },
+                { x: 2, y: 2 },
+                { x: 4, y: 3 },
+                { x: 6, y: 4 },
+                { x: 8, y: 5 }
+            ]
+        ]
+    }
+    var options = {
+        axisX: {
+            type: Chartist.AutoScaleAxis,
+        },
+        low: 0,
+        width: "22%",
+        height: "25%",
+        showLine: true,
+        showLabel: true,
+        showPoint: true,
+    }
+    chart.update(data, options);
 }
